10341f4fb5b02c247c5f1c330877e248b8de7d11
raissa-k <raissakuzerdev@gmail.com>
2022-04-19 17:24:43 -0300

    Added Easter Egg

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const jokeDad = document.getElementById('jokeDad')
 toggle.addEventListener('click', changeJoke)
 document.getElementById('getNorris').addEventListener('click', fetchNorris)
 document.getElementById('getDad').addEventListener('click', fetchDad)
+document.getElementById('getLeon').addEventListener('click', fetchLeon)
+document.getElementById('bonus').addEventListener('change', hideStuff)
 
 function fetchNorris(){
   const url = 'https://api.chucknorris.io/jokes/random?category=animal,career,fashion,history,movie,music,science,sport,travel'
@@ -18,16 +20,30 @@ function fetchNorris(){
       });
   }
 
+  function fetchLeon(){
+    const url = 'https://api.chucknorris.io/jokes/random?name=Leon&category=dev'
+    
+      fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          document.getElementById('jokeLeon').textContent = data.value
+          console.log(data)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+    }
+
   async function fetchDad() {
-    const response = await fetch("https://cors-everywhere.herokuapp.com/http://icanhazdadjoke.com", {
-      headers: {
-        Accept: "application/json",
-      },
-    });
-    const data = response.json();
-    jokeDad.textContent = data.joke
-    return data;
-  }
+    const jokeRes = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+      const joke = await jokeRes.json();
+      jokeDad.innerHTML = joke.joke;
+    }
+    
   
 
 function changeJoke(){
@@ -39,3 +55,13 @@ function changeJoke(){
   document.getElementById('norris').classList.toggle('hide');
   document.getElementById('dad').classList.toggle('hide');
 }
+
+function hideStuff(){
+  document.getElementById('regular').classList.toggle('hide');
+  document.getElementById('leon').classList.toggle('hide')
+  document.getElementById('container').classList.toggle('leon');
+  document.getElementById('joke').classList.toggle('hide');
+  document.getElementById('leonJoke').classList.toggle('hide');
+}
+
+fetchDad();
